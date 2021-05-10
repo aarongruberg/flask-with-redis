53326a9453efa4e3e135fac5a419972241dda502
Aaron Gruberg <aarongruberg@Aarons-MBP.attlocal.net>
2021-05-10 15:49:41 -0700

    In app.py, added connection test to redis private service https://redis.io on port 10000

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 
-# Example with index.html
+# Added imports for redis
 from flask import Flask, render_template
+import redis
 
 app = Flask(__name__)
+
+# Added redis database connection test
+try:
+    conn = redis.StrictRedis(
+        host='https://redis.io',
+        port=10000,
+        #password='YOUR_PASSWORD')
+    print conn
+    conn.ping()
+    print 'Connected!'
+except Exception as ex:
+    print 'Error:', ex
+    exit('Failed to connect, terminating.')
+
 
 @app.route('/')
 def index():
