87f2a56aa6b483201d5bb5d35970b017a82081f0
Aaron Gruberg <aarongruberg@Aarons-MBP.attlocal.net>
2021-05-10 18:19:48 -0700

    undid some things I was testing earlier

--- app.py
+++ app.py
@@ -1,32 +1,32 @@
 
 # Added imports for redis
 from flask import Flask, render_template
-#import redis
+import redis
 
 app = Flask(__name__)
 
-# Added redis database connection test
-# try:
-#     conn = redis.StrictRedis(
-#         host='redis-test-kris',
-#         port=10000)
-#         #password='YOUR_PASSWORD')
-#
-#     #print(conn)
-#     app.logger.info('connecting')
-#     app.logger.info(conn)
-#     conn.ping()
-#     print('Connected!')
-# except Exception as ex:
-#     print('Error:', ex)
-#     exit('Failed to connect, terminating.')
+# test logging strings
+app.logger.info('testing')
 
-raise Exception('Oops')
+# Added redis database connection test
+try:
+    conn = redis.StrictRedis(
+        host='redis-test-kris',
+        port=10000)
+        #password='YOUR_PASSWORD')
+
+    #print(conn)
+    app.logger.info('connecting')
+    app.logger.info(conn)
+    conn.ping()
+    print('Connected!')
+except Exception as ex:
+    print('Error:', ex)
+    exit('Failed to connect, terminating.')
+
+#raise Exception('Oops')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# testing this function
-app.run()
